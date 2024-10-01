0594055733569ecc1d804cf1d4eacef1b00e899a
CesarAGonz <76890038+CesarAGonz@users.noreply.github.com>
2024-10-01 05:16:30 -0600

    import interface from the new file

--- src/components/sections/career/CareerItem.tsx
+++ src/components/sections/career/CareerItem.tsx
@@ -1,17 +1,11 @@
-interface CareerItemProps {
-    title: string;
-    company: string;
-    duration: string;
-    description: string;
-  }
-  
-  export default function CareerItem({ title, company, duration, description }: CareerItemProps) {
-    return (
-      <div style={{ marginBottom: "2rem" }}>
-        <h3 style={{ fontSize: "1.5rem", fontWeight: "bold", color: "#00b8d4" }}>{title}</h3>
-        <p style={{ fontSize: "1.125rem" }}>{company} | {duration}</p>
-        <p>{description}</p>
-      </div>
-    );
-  }
-  
+import { CareerItem as CareerItemInterface } from '../../../configs/interfaces';
+
+export default function CareerItem({ title, company, duration, description }: CareerItemInterface) {
+  return (
+    <div className="career-card">
+      <h3>{title}</h3>
+      <p className="company-duration">{company} | {duration}</p>
+      <p>{description}</p>
+    </div>
+  );
+}

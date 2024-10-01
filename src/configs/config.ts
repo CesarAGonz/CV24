e8b4e7069d9c7dd2d8600f88a01b58cadacbfbaf
CesarAGonz <76890038+CesarAGonz@users.noreply.github.com>
2024-10-01 05:16:05 -0600

    add more configs

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,1 +1,38 @@
+import { CareerItem } from './interfaces';
+
+
 export const navLinks: string[] = ["About", "Career", "Skills", "Projects", "Contact"];
+
+export const careerData: CareerItem[] = [
+    {
+      title: "Application Developer",
+      company: "PSL Group",
+      duration: "October 2022 — September 2024",
+      description: "Software developer at a multinational company, member of a development team for global pharmaceutical news and analytics service platforms.",
+    },
+    {
+      title: "Full-stack Developer",
+      company: "Solo Trucking",
+      duration: "November 2021 — September 2022",
+      description: "Software developer at an American startup, member of the development team for the web and mobile application.",
+    },
+    {
+      title: "Full-stack Developer",
+      company: "Brounie",
+      duration: "September 2021 — October 2021",
+      description: "Full-stack web developer at a startup specializing in custom software solutions and technology implementations. Hired on a temporary contract to complete a project.",
+    },
+    {
+      title: "IT Manager",
+      company: "Stamping Steel and Trucks",
+      duration: "July 2020 — September 2021",
+      description: "Manager of an IT department at a metallurgical company that required development services for an ERP system and other related products. Additionally, I implemented technology solutions for security measures and COVID-19 prevention protocols.",
+    },
+    {
+      title: "IT Analyst",
+      company: "Stamping Steel and Trucks",
+      duration: "July 2019 — July 2020",
+      description: "Manager of an IT department at a metallurgical company that required development services for an ERP system and other related products. Additionally, I implemented technology solutions for security measures and COVID-19 prevention protocols.",
+    },
+  ];
+  

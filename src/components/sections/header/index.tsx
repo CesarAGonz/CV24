e3d585cf25df2faf7784e653eecd5fa005887b63
CesarAGonz <76890038+CesarAGonz@users.noreply.github.com>
2024-10-03 04:21:06 -0600

    Header styles movile dising

--- src/components/sections/header/index.tsx
+++ src/components/sections/header/index.tsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import { Link } from "@chakra-ui/react";
+// import MenuIcon from '@mui/icons-material/Menu';
 import './HeaderStyles.css';
 import { navLinks } from '../../../configs/config';
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header>
       <div className="header-container">
-        <Link href="#" className="header-link">Cesar Gonzalez</Link>
-        <nav className="header-nav">
+        <Link href="#" className="header-link">CesarDev</Link>
+        <button className={`hamburger ${isOpen ? 'open' : ''}`} onClick={toggleMenu} aria-label="Menu">
+          {/* <MenuIcon className="menu-icon" /> */}
+        </button>
+        <nav className={`header-nav ${isOpen ? 'active' : ''}`}>
           {navLinks.map((item, index) => (
             <Link key={index} href="#" className="header-nav-link">
               {item}
